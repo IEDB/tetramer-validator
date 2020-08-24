35f1f2919c9f7306be650221bd8a22c62b18c143
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-08-24 12:29:35 -0700

    Initial Flask Server

---
+++ demo.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for
+from . import validate
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def start():
+    return render_template("test.html")
+
+
+@app.route("/output", methods=["POST"])
+def output():
+    if request.method == "POST":
+        pep_seq = request.form["pep_seq"]
+        mod_pos = request.form["mod_pos"]
+        mod_type = request.form["mod_type"]
+        mhc_name = request.form["mhc_name"]
+        statement = validate.validate(
+            pep_seq=pep_seq, mod_pos=mod_pos, mod_type=mod_type, mhc_name=mhc_name
+        )
+        return render_template(
+            "test1.html",
+            pep_seq=pep_seq,
+            mod_pos=mod_pos,
+            mod_type=mod_type,
+            mhc_name=mhc_name,
+            statement=statement,
+        )
