4a83b8e06955f932bdd2c24285be6fffdd037c41
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-08-28 13:32:08 -0700

    Success message

--- demo.py
+++ demo.py
@@ -4,14 +4,29 @@ from . import validate
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def output():
     if request.args:
-       pep_seq = request.args['pep_seq']
-       mod_pos = request.args['mod_pos']
-       mod_type = request.args['mod_type']
-       mhc_name = request.args['mhc_name']
-       statement = validate.validate(pep_seq = pep_seq, mod_pos= mod_pos, mod_type = mod_type, mhc_name = mhc_name)
-       return render_template("test1.html", pep_seq = pep_seq, mod_pos=mod_pos, mod_type=mod_type, mhc_name=mhc_name, statement=statement)
+        pep_seq = request.args["pep_seq"]
+        mod_pos = request.args["mod_pos"]
+        mod_type = request.args["mod_type"]
+        mhc_name = request.args["mhc_name"]
+        statement = validate.validate(
+            pep_seq=pep_seq, mod_pos=mod_pos, mod_type=mod_type, mhc_name=mhc_name
+        )
+        if not statement:
+            statement = "Success! This input is valid"
+        return render_template(
+            "test1.html",
+            pep_seq=pep_seq,
+            mod_pos=mod_pos,
+            mod_type=mod_type,
+            mhc_name=mhc_name,
+            statement=statement,
+        )
     else:
-       return render_template("test1.html", pep_seq = "", mod_pos="", mod_type="", mhc_name="", statement="")
+        print("else")
+        return render_template(
+            "test1.html", pep_seq="", mod_pos="", mod_type="", mhc_name="", statement=""
+        )
