d5109611cb96994a364f3b2ffaf6f110d4f76d68
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-09-18 14:52:15 -0700

    Added parsing of csv files and generating error messaging file.

--- tetramer_validator/parse_tables.py
+++ tetramer_validator/parse_tables.py
@@ -14,22 +14,38 @@ def parse_excel_file(filename):
     header = [entry.value for entry in ws[1]]
 
     rows = ws.iter_rows(min_row=2)
-    cwd = getcwd()
-    new_file = path.join(cwd, "messages.txt")
 
-    messages = open(new_file, "w+")
+
+    messages = []
     for row in rows:
         message = validate(
-            pep_seq=row[0].value, mhc_name=row[3].value, mod_type=row[1].value, mod_pos=row[2].value
+            pep_seq=row[0].value, mhc_name=row[3].value,
+            mod_type=row[1].value, mod_pos=row[2].value
         )
         if message:
-            messages.write(message)
+            messages.append(message)
         else:
-            messages.write(f"Peptide sequence {row[0].value} is valid")
-        messages.write("\n")
-    messages.close()
+            messages.append(f"Peptide sequence {row[0].value} is valid")
+    generate_messages_txt(messages)
     return "Please see messages.txt for any errors"
 
 def parse_csv_tsv(filename, delimiter):
-    with open(filename, "r") as file_obj:
+    with open(filename, "r", encoding='utf-8-sig') as file_obj:
         reader = csv.DictReader(file_obj, delimiter=delimiter)
+        messages = []
+        for entry in reader:
+            messaage = validate(pep_seq = entry["Peptide Sequence"],
+                   mhc_name=entry["MHC Name"],mod_type = entry["Modification Type"],
+                   mod_pos=entry["Modification Position"])
+            messages.append(message)
+        generate_messages_txt(messages)
+        return "Please see messages.txt for any errors"
+        
+def generate_messages_txt(messages, filename="messages.txt"):
+    cwd = getcwd()
+    new_file = path.join(cwd, filename)
+    message_file = open(new_file, "w+")
+    for message in messages:
+        message_file.write(message)
+        message_file.write("\n")
+    message_file.close()
