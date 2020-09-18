c989329b93f95fdceb1225278564d17c2694fafa
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-09-17 17:12:43 -0700

    Added entry point "cli.py"

--- setup.py
+++ setup.py
@@ -20,4 +20,8 @@ setuptools.setup(
     install_requires=["openpyxl"],
     python_requires=">=3.6",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "tv = tetramer_validator.cli:main",
+        ],
 )
