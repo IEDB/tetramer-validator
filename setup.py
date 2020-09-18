76fbd3551cdc7c4c2343a61a58b8d0f451997b2c
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-09-18 12:09:20 -0700

    Uncommented URL line in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small package to validate tetramers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/IEDB/tetramer-validator",
+    url="https://github.com/IEDB/tetramer-validator",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
