78ba28baa765753cd3537910a474c2e5c57a3aa6
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-09-23 20:06:58 -0700

    Implemented Bloodhound with synonyms for PTM_list

--- tetramer_validator/static/MHC_display.js
+++ tetramer_validator/static/MHC_display.js
@@ -1,5 +1,7 @@
 var MHC_engine = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace("Label", "IEDB Label", "synonyms" ),
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace([
+    "Label", "IEDB Label", "synonyms"
+  ]),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   prefetch: {
     url: "/data/molecule.json",
@@ -13,13 +15,12 @@ var MHC_engine = new Bloodhound({
 MHC_engine.initialize();
 
 // instantiate the typeahead UI
-$('#MHC_display .form-control').typeahead(
-  { hint: true,
-    highlight: true,
-    minLength: 1,
-    autoselect: true,
-  },
-  {
+$('#MHC_display .form-control').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1,
+  autoselect: true,
+}, {
   name: 'MHC_name',
   displayKey: function(mhc) {
     return mhc.Label
