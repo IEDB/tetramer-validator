3b29e08292ba2a600aa9e90d769d3333fb4545ca
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-09-25 17:12:01 -0700

    Added structured errors

--- tetramer_validator/validate.py
+++ tetramer_validator/validate.py
@@ -11,10 +11,14 @@ with open(molecule_file) as fh:
 PTM_file = "data/PTM_list.tsv"
 PTM_file = path.join(here, PTM_file)
 
-
+errors = []
 with open(PTM_file) as fh_1:
     reader = csv.DictReader(fh_1, delimiter="\t")
     PTM_display = [name["display_name"] for name in reader]
+
+
+def generate_problem_table(level, rule_name, value, instructions, fix=None):
+    errors.append(locals())
 
 
 def validate(pep_seq, mhc_name, mod_type=None, mod_pos=None):
@@ -35,41 +39,105 @@ def validate(pep_seq, mhc_name, mod_type=None, mod_pos=None):
 def properNumArguments(args):
     """Checks for proper number and combination of arguments for validate function"""
 
-    incorrect_num_str = "Incorrect number of arguments:"
+    incorrect_num_str = "IncorrectNumArgs"
 
     if not args["pep_seq"]:
-        return "".join(incorrect_num_str, "Peptide sequence is required")
+        generate_problem_table(
+            level="error",
+            rule_name=incorrect_num_str,
+            value=args["pep_seq"],
+            instructions="Enter peptide sequence",
+        )
+
     if not args["mhc_name"]:
-        return "".join(incorrect_num_str, "MHC molecule required")
+        generate_problem_table(
+            level="error",
+            rule_name=incorrect_num_str,
+            value=args["mhc_name"],
+            instructions="Enter MHC molecule",
+        )
+
     if args["mod_pos"] and not args["mod_type"]:
-        return (
-            "Incorrect number of arguments: Modificiation position provided"
-            " but no modification type"
+        generate_problem_table(
+            level="error",
+            rule_name=incorrect_num_str,
+            value=args["mod_type"],
+            instructions="Provide modificiation type(s)",
         )
+
     if args["mod_type"] and not args["mod_pos"]:
-        return (
-            "Incorrect number of arguments: Modification type provided"
-            " but not modification position"
+        generate_problem_table(
+            level="error",
+            rule_name=incorrect_num_str,
+            value=args["mod_pos"],
+            instructions="Provide modificiation position(s)",
         )
 
-    null_str = "NULL"
-    if null_str in args.values():
-        return "NULL value entered. If there is no need for a particular value, please leave blank."
-
-    return None
+    null_rule_name = "NullValueEntered"
+    null_strs = [
+        "#N/A",
+        "#N/A",
+        "N/A",
+        "#NA",
+        "-1.#IND",
+        "-1.#QNAN",
+        "-NaN",
+        "-nan",
+        "1.#IND",
+        "1.#QNAN",
+        "NA",
+        "NULL",
+        "NaN",
+        r"n/a",
+        "nan",
+        "null",
+        "<NA>",
+        "N/A",
+    ]
+    for (param, value) in args.items():
+        if value in null_strs:
+            if param == "pep_seq":
+                generate_problem_table(
+                    level="error",
+                    rule_name=null_rule_name,
+                    value=value,
+                    instructions="Please do not enter any null string."
+                    " Peptide sequence is required",
+                )
+            elif param == "mhc_name":
+                generate_problem_table(
+                    level="error",
+                    rule_name=null_rule_name,
+                    value=value,
+                    instructions="Please do not enter any null string."
+                    " MHC molecule is required",
+                )
+            else:
+                generate_problem_table(
+                    level="error",
+                    rule_name=null_rule_name,
+                    value=value,
+                    fix="",
+                    instructions="Please do not enter any null string."
+                    " Leave blank if needed",
+                )
 
 
 def validate_amino_acids(pep_seq):
     """Thanks to Austin Crinklaw.  Simple function to validate if given string has valid amino
     acids letters"""
+
+    aa_rule_name = "UnrecognizedAminoAcid"
     pattern = re.compile(r"[^A|C|D|E|F|G|H|I|K|L|M|N|P|Q|R|S|T|V|W|X|Y]", re.IGNORECASE)
     has_amino_acids = pattern.findall(pep_seq)
     if has_amino_acids:
-        return (
-            f"Unrecognized amino acid: Peptide sequence {pep_seq} has characters {has_amino_acids} "
-            "that are not amino acids"
+        generate_problem_table(
+            level="error",
+            rule_name=aa_rule_name,
+            value=pep_seq,
+            instructions=f"The peptide sequence has characters {has_amino_acids}"
+            " that are not amino acids",
         )
-    return None
 
 
 def format_mod_info(mod_pos, mod_type):
@@ -86,12 +154,19 @@ def format_mod_info(mod_pos, mod_type):
 
 def validate_PTM_names(mod_types):
     """Check if given list of modification types match up to MOD list"""
+
+    invalid_PTM_rule = "InvalidPTMtype"
     for type in mod_types:
         if type not in PTM_display:
-            return f"{type} is not a valid modification type"
+            generate_problem_table(
+                level="error",
+                rule_name=invalid_PTM_rule,
+                value=type,
+                instructions="Please choose a post-translational type from the autocomplete list",
+            )
 
 
-def validate_mod_pos_syntax(positions):
+def validate_mod_pos_syntax(pep_seq, positions):
     """Given list of modification positions, validates list against syntax of amino acid followed
     by position number (e.g. ['N1', 'N100'])"""
 
@@ -102,29 +177,59 @@ def validate_mod_pos_syntax(positions):
     reversed_pattern = re.compile(
         r"\d+[A|C|D|E|F|G|H|I|K|L|M|N|P|Q|R|S|T|V|W|X|Y]", re.IGNORECASE
     )
+    just_digits = "JustDigits"
+    reversed = "ReverseAminoAcid"
+    general = "GeneralModPos"
     for pos in positions:
         if re.fullmatch(main_pattern, pos) is None:
             formatted_string = (
                 f"{pos} is not a valid modification position."
                 "Modification Position field should be a comma separated list of amino acid "
-                "letter followed by position number (e.g. F1, S10, S300)"
+                "letter followed by position number (e.g. F1, S10, S300) "
             )
+
             if re.fullmatch(pattern=digits, string=pos):
-                return formatted_string + "This input is just digit(s)"
+                if int(pos) > len(pep_seq):
+                    generate_problem_table(
+                        level="error",
+                        rule_name=just_digits,
+                        value=pos,
+                        instructions=formatted_string + "This input is just digit(s)",
+                    )
+                else:
+                    generate_problem_table(
+                        level="error",
+                        rule_name=just_digits,
+                        fix=str(pep_seq[int(pos) - 1] + pos),
+                        value=pos,
+                        instructions=formatted_string
+                        + "Enter amino acid before digit(s)",
+                    )
+
             elif re.fullmatch(pattern=reversed_pattern, string=pos):
-                return (
-                    formatted_string
-                    + "This input has syntax reversed syntax."
-                    + f"Should be {pos[-1]}{pos[:-1]}"
+                generate_problem_table(
+                    level="error",
+                    rule_name=reversed,
+                    fix=f"{pos[-1]}{pos[:-1]}",
+                    value=pos,
+                    instructions=formatted_string
+                    + "Enter amino acid followed by position.",
                 )
+
             else:
-                return formatted_string
+                generate_problem_table(
+                    level="error",
+                    rule_name=general,
+                    value=pos,
+                    instructions=formatted_string,
+                )
     return None
 
 
 def validate_peptide(pep_seq, mod_pos, mod_type):
     """Main helper function to validate.py, checks for validation of peptide sequence, modification
     position, and modification type"""
+
     statement = validate_amino_acids(pep_seq)
 
     if statement:
@@ -132,72 +237,92 @@ def validate_peptide(pep_seq, mod_pos, mod_type):
 
     positions, mod_types = format_mod_info(mod_pos)
 
+    trailing_rule_name = "TrailingCharacters"
     trailing_characters = re.findall(
         r"[^A|C|D|E|F|G|H|I|K|L|M|N|P|Q|R|S|T|V|W|X|Y\d]+$", positions
     )
     if trailing_characters:
-        return (
-            f"FormatError: {trailing_characters} at the end of input {mod_pos}"
-            " are unrecognized"
+        generate_problem_table(
+            level="error",
+            rule_name=trailing_rule_name,
+            value=mod_pos,
+            instructions=f"Remove {trailing_characters} from modification position",
         )
 
     positions = positions.split(",")
     mod_types = mod_types.split(",")
 
-    statement = validate_mod_pos_syntax(positions)
-    if statement:
-        return statement
+    validate_mod_pos_syntax(pep_seq, positions)
 
     num_mod_types = len(mod_types)
     num_mod_pos = len(positions)
-    if num_mod_pos != num_mod_types:
-        return (
-            f"MismatchError: There are {num_mod_pos} positions but {num_mod_types}"
-            " modification types"
+    mod_num_mismatch = "NumModPosTypeMismatch"
+    if num_mod_pos < num_mod_types:
+        generate_problem_table(
+            level="warn",
+            rule_name=mod_num_mismatch,
+            value=mod_pos,
+            instructions="Decrease number of modification types"
+            " or increase number of modification positions",
+        )
+    if num_mod_types < num_mod_pos:
+        generate_problem_table(
+            level="warn",
+            rule_name=mod_num_mismatch,
+            value=mod_type,
+            instructions="Decrease number of modification positions"
+            " or increase number of modification types",
         )
 
-    statement = validate_PTM_names(mod_types)
-    if statement:
-        return statement
+    validate_PTM_names(mod_types)
 
-    statement = validate_mod_pos(pep_seq, positions)
-    if statement:
-        return statement
-
-    return None
+    validate_mod_pos(pep_seq, positions)
 
 
-def validate_pep_seq_mhc_name(pep_seq, mhc_name):
+def validate_pep_seq_mhc_name(mhc_name):
     """Check if given MHC name match up to MRO name"""
+    invalid_MHC_rule = "InvalidMHCmol"
     if mhc_name not in molecules:
-        return f"{mhc_name} is not a valid MHC molecule name"
-    return None
+        generate_problem_table(
+            level="error",
+            rule_name=invalid_MHC_rule,
+            value=mhc_name,
+            instructions="Enter MHC molecule from prepopulated list",
+        )
 
 
 def validate_mod_pos(pep_seq, positions):
     """Validates the list of modification positions (in proper syntax) for peptide sequence entered
     as occuring at the stated position."""
+
+    pos_pep_seq_rule = "AminoAcidPosMismatch"
     try:
         for pos in positions:
             if len(pos) >= 2:
                 position = "".join(pos[1:])
                 position = int(position) - 1
                 if pep_seq[position] is not pos[0]:
-                    part_one = f"MismatchError: This peptide sequence {pep_seq} "
-                    part_two = f"does not contain {pos[0]} at position {pos[1:]}"
+                    part_one = "This peptide sequence "
+                    part_two = f"does not contain {pos[0]} at position {pos[1:]}. "
                     result = part_one + part_two
-                    return result
-            else:
-                return f"""There are {len(pos)} characters in one of the modification positions"""
+                    generate_problem_table(
+                        level="error",
+                        rule_name=pos_pep_seq_rule,
+                        value=pos,
+                        instructions=result
+                        + "Enter a amino acid letter and matching position from peptide sequence",
+                    )
 
-    except IndexError as i:
-        formatted_string_one = (
-            f"IndexError. {position + 1} is greater than number of amino acids in "
+    except IndexError:
+        index_rule = "PosGreaterPepLen"
+        formatted_string = (
+            f"IndexError. {position + 1} is greater than number of"
+            " amino acids in peptide sequence. "
         )
-        formatted_string_two = f"peptide sequence {pep_seq}."
-        system_err_pre = "Here is the error message from the system: "
-        final_string = (
-            formatted_string_one + formatted_string_two + "\n" + system_err_pre + str(i)
+        generate_problem_table(
+            level="error",
+            rule_name=index_rule,
+            value=int(pos[1:]),
+            instructions=formatted_string
+            + "Enter position that is less than length of peptide sequence and more than 0.",
         )
-        return final_string
-    return None
