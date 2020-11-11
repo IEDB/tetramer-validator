e7103b16b5e06617f2e1ce102b8b5bb16ccfac96
Apurva Mody <28663318+apmody@users.noreply.github.com>
2020-11-11 14:09:44 -0800

    Rearranged columns in example files, formatting changes

--- tetramer_validator/server.py
+++ tetramer_validator/server.py
@@ -34,6 +34,7 @@ app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 if not os.path.isdir("./downloads"):
     os.mkdir("./downloads")
 
+
 @app.route("/", methods=["GET"])
 def output():
     if request.args:
@@ -214,5 +215,7 @@ def results(path):
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-#if __name__ == '__main__':
+
+
+# if __name__ == '__main__':
 #    app.run(host='0.0.0.0', debug=True)
